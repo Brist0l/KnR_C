c078422fd1b54ef7effa17da73174795569cfdc2
Bris0l <gautamthegreat21@gmail.com>
2024-08-05 12:15:57 +0530

    Finished Chap 3

---
+++ 3Control_Flow/3.2If_Else/zero_non_zero.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	if (0)
+		printf("In If");
+	else
+		printf("In else");
+
+	if (-100)
+		printf("In If");
+	else
+		printf("In else");
+}
+
+// Any non-zero value is true , whilist 0 is always false
